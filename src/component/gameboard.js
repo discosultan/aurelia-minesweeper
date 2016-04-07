5beac9bbba57590a0d94e45c4cc1c1801944a283
Discosultan <j2v@neti.ee>
2016-04-07 20:03:23 +0300

    Remove hammer; add touch support

--- src/component/gameboard.js
+++ src/component/gameboard.js
@@ -12,6 +12,10 @@ export class Gameboard {
 
   @bindable settings = null;
 
+  _ongoingTouchSet = {};
+  _doubleTapTimeToChord = 200; // Milliseconds.
+  _holdTimeToFlag = 400;
+
   constructor(eventAggregator) {
     this._eventAggregator = eventAggregator;
     this._mouseUpCallback = this._onMouseUp.bind(this);
@@ -51,24 +55,20 @@ export class Gameboard {
   onSquareMouseDown(square, evt) {
     if (evt.which === MouseButton.Left) {
       this.isMouseLeftDown = true;
-      this.isPressValid = true;
       this.minesweeper.pressSquare(square, this._isChording());
+      this.isPressValid = false;
     } else if (evt.which === MouseButton.Right) {
       this.isMouseRightDown = true;
       if (this.isMouseLeftDown) {
-        this.isPressValid = true;
         this.minesweeper.pressSquare(square, this._isChording());
+        this.isPressValid = false;
       }
     }
   }
 
   onSquareMouseUp(square, evt) {
     if (this.isMouseLeftDown && (evt.which === MouseButton.Left || evt.which === MouseButton.Right)) {
-      let previousState = this.minesweeper.state;
-      this.minesweeper.openSquare(square, this._isChording());
-      let currentState = this.minesweeper.state;
-      if (previousState !== currentState)
-        this._eventAggregator.publish(new GameStateChangedEvent(this.minesweeper, previousState, currentState));
+      this._openSquare(square, this._isChording());
       this.isPressValid = false;
     }
   }
@@ -86,6 +86,57 @@ export class Gameboard {
     evt.preventDefault();
     if (!this.isMouseLeftDown && evt.which && evt.which === MouseButton.Right)
       this.minesweeper.toggleSquareFlag(square);
+  }
+
+  onSquareTouchStart(square, evt) {
+    evt.preventDefault();
+    let touches = evt.changedTouches;
+    for (let i = 0; i < touches.length; i++) {
+      let touch = touches[i];
+      this._ongoingTouchSet[touch.identifier] = true;
+      (identifier =>
+        window.setTimeout(() => {
+          this.minesweeper.toggleSquareFlag(square);
+          delete this._ongoingTouchSet[identifier];
+        }, this._holdTimeToFlag)
+      )(touch.identifier);
+    }
+  }
+
+  onSquareTouchEnd(square, evt) {
+    evt.preventDefault();
+    let touches = evt.changedTouches;
+    for (let i = 0; i < touches.length; i++) {
+      let touch = touches[i];
+      if (this._ongoingTouchSet[touch.identifier]) {
+        if (this._doubleTapActive) { // Chord on double tap.
+          this.minesweeper.pressSquare(square, true);
+          this._openSquare(square, true);
+          this._doubleTapActive = false;
+        } else {
+          this._doubleTapActive = true;
+          window.setTimeout(() => this._doubleTapActive = false, this._doubleTapTimeToChord);
+        }
+        this.minesweeper.pressSquare(square, false);
+        this._openSquare(square, false);
+        delete this._ongoingTouchSet[touch.identifier];
+      }
+    }
+  }
+
+  onSquareTouchCancel(square, evt) {
+    evt.preventDefault();
+    var touches = evt.changedTouches;
+    for (let i = 0; i < touches.length; i++)
+      delete this._ongoingTouchSet[touches[i].identifier];
+  }
+
+  _openSquare(square, chord) {
+    let previousState = this.minesweeper.state;
+    this.minesweeper.openSquare(square, chord);
+    let currentState = this.minesweeper.state;
+    if (previousState !== currentState)
+      this._eventAggregator.publish(new GameStateChangedEvent(this.minesweeper, previousState, currentState));
   }
 
   _isChording() { return this.isMouseLeftDown && this.isMouseRightDown; }
